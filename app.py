39a1d5cfdbeedebf731a9af94f4f230a1c3de3cb
taanish M <taanishraj732004@gmail.com>
2025-02-24 14:54:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 # Model path
 model_path = "trained_plant_disease_model.keras"
-gdrive_url = "https://drive.google.com/drive/u/0/folders/1KSkBlZ9I23wvxXmwmwkDbON_XSF-84PC"  # Modified Google Drive link
+gdrive_url = "https://drive.google.com/uc?id=13FPoqxOZE9_V_zP6cErcVfaBm7tulBf5"  # Modified Google Drive link
 
 # Function to download the model if not found
 def download_model():
